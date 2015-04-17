f59c5a9602e3bb88ba2495131da238ac183d5525
zichent <tangzichen.plus@gmail.com>
2015-04-16 21:58:32 -0400

    Update Cruiser.java

--- src/battleship/Cruiser.java
+++ src/battleship/Cruiser.java
@@ -2,12 +2,18 @@ package battleship;
 
 public class Cruiser extends Ship {
 //Describes a ship of length 3.
+	/**
+	 * build a ship object with length 3
+	 */
 	public Cruiser() {
 		length=3;
 		// TODO Auto-generated constructor stub
 		//hit=new boolean[]{false,false,false,false};
 		hit=new boolean[]{false,false,false};
 	}
+	/**
+	 * return the type of the object
+	 */
 	@Override
 	String getShipType() {
 		// TODO Auto-generated method stub
@@ -15,3 +21,4 @@ public class Cruiser extends Ship {
 	}
 
 }
+
