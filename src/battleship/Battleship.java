681df18ce5c01b9b103418f03d7e25cee941601d
zichent <tangzichen.plus@gmail.com>
2015-04-16 21:57:13 -0400

    Update Battleship.java

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -2,15 +2,22 @@ package battleship;
 
 public class Battleship extends Ship {
 //Describes a ship of length 4
+	/**
+	 * bulid a ship object with length 4
+	 */
 	public Battleship() {
 		// TODO Auto-generated constructor stub
 		length=4;
 		//hit=new boolean[]{false,false,false,false};
 		hit=new boolean[]{false,false,false,false};
 	}
+	/**
+	 * return the type of the ship
+	 */
 	@Override
 	String getShipType() {
 		// TODO Auto-generated method stub
 		return "battleship";
 	}
 }
+
