273ec27d385c4c1631884fefe179ee0a7ab4b4f4
zichent <tangzichen.plus@gmail.com>
2015-04-16 22:02:08 -0400

    Update Submarine.java

--- src/battleship/Submarine.java
+++ src/battleship/Submarine.java
@@ -2,12 +2,18 @@ package battleship;
 
 public class Submarine extends Ship{
 //Describes a ship of length 1
+	/**
+	 * create a ship object with length 1.
+	 */
 	public Submarine() {
 		// TODO Auto-generated constructor stub
 		length=1;
 		//hit=new boolean[]{false,false,false,false};
 		hit=new boolean[]{false };
 	}
+	/**
+	 * return type submarine.
+	 */
 	@Override
 	String getShipType() {
 		// TODO Auto-generated method stub
