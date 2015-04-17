dd675c39ba89a63fe6c73c046848d561df77da77
zichent <tangzichen.plus@gmail.com>
2015-04-16 21:58:53 -0400

    Update Destroyer.java

--- src/battleship/Destroyer.java
+++ src/battleship/Destroyer.java
@@ -2,13 +2,18 @@ package battleship;
 
 public class Destroyer extends Ship{
 // Describes a ship of length 2
+	/**
+	 * build a ship object with length 2
+	 */
 	public Destroyer() {
 		// TODO Auto-generated constructor stub
 		length=2;
 		//hit=new boolean[]{false,false,false,false};
 		hit=new boolean[]{false,false};
 	}
-
+    /**
+     * return the type of the object
+     */
 	@Override
 	String getShipType() {
 		// TODO Auto-generated method stub
